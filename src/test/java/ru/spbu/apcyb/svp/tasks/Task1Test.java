f495d8bde6c88f3694f29776df72da58aa94016f
Chernovuk <cherpash20@gmail.com>
2023-12-12 20:41:00 +0300

    Refactored inputSumForExchange, inputNominals and modifyNominals tests

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
@@ -14,16 +14,21 @@ import org.junit.jupiter.api.Test;
 class Task1Test {
 
   @Test
-  void inputSumForExchange() {
-
+  void inputSumForExchange_300000000() {
     System.setIn(new ByteArrayInputStream("300000000".getBytes()));
     assertEquals(300000000, Task1.inputSumForExchange());
     System.setIn(System.in);
+  }
 
+  @Test
+  void inputSumForExchange_String() {
     System.setIn(new ByteArrayInputStream("fff".getBytes()));
     assertThrows(IllegalArgumentException.class, Task1::inputSumForExchange);
     System.setIn(System.in);
+  }
 
+  @Test
+  void inputSumForExchange_Negative() {
     System.setIn(new ByteArrayInputStream("-3".getBytes()));
     assertThrows(IllegalArgumentException.class, Task1::inputSumForExchange);
     System.setIn(System.in);
@@ -31,14 +36,17 @@ class Task1Test {
 
 
   @Test
-  void inputNominals() {
+  void inputNominals_2_1_5() {
 
     System.setIn(new ByteArrayInputStream("2 1 5".getBytes()));
     long[] expected = {2, 1, 5};
     long[] actual = Task1.inputNominals();
     System.setIn(System.in);
     assertArrayEquals(expected, actual);
+  }
 
+  @Test
+  void inputNominals_2_String() {
     System.setIn(new ByteArrayInputStream("2 avc".getBytes()));
     boolean thrown = false;
     try {
@@ -51,43 +59,39 @@ class Task1Test {
   }
 
   @Test
-  void modifyNominals() {
-
+  void modifyNominals_sum_7_noms_6_2_5_6_7_2() {
     long sumForExchange = 7;
     long[] nominals = {6, 2, 5, 6, 7, 2};
     long[] expected = {2, 5, 6, 7};
     long[] actual = Task1.modifyNominals(sumForExchange, nominals);
     assertArrayEquals(expected, actual);
-
-    boolean thrown = false;
-    long[] nominals1 = {-2, 1, 6};
-    try {
-      Task1.modifyNominals(sumForExchange, nominals1);
-    } catch (Exception e) {
-      thrown = true;
-    }
-    assertTrue(thrown);
-
-    thrown = false;
-    long[] nominals2  = {5, 2, 10, 8, 1};
-    try {
-      Task1.modifyNominals(sumForExchange, nominals2);
-    }  catch (Exception e) {
-      thrown = true;
-    }
-    assertTrue(thrown);
   }
 
   @Test
-  void modifyNominals4() {
+  void modifyNominals_Negative() {
+    long sumForExchange = 7;
+    long[] nominals = {-2, 1, 6};
+    assertThrows(IllegalArgumentException.class,
+            () -> Task1.modifyNominals(sumForExchange, nominals));
+  }
 
-    long numb = 15;
-    long[] val = {2, 3, 5};
+  @Test
+  void modifyNominals_ValueGreaterThanSum_7() {
+    long sumForExchange = 7;
+    long[] nominals  = {5, 2, 10, 8, 1};
+    assertThrows(IllegalArgumentException.class,
+            () -> Task1.modifyNominals(sumForExchange, nominals));
+  }
 
-    long[] exp = {2, 3, 5};
-    long[] act = Task1.modifyNominals(numb, val);
+  @Test
+  void modifyNominalsToStayTheSame() {
+    long sumForExchange = 15;
+    long[] nominals = {2, 3, 5};
 
-    assertArrayEquals(exp, act);
+    long[] expected = {2, 3, 5};
+    long[] actual = Task1.modifyNominals(sumForExchange, nominals);
+
+    assertArrayEquals(expected, actual);
   }
 
 
